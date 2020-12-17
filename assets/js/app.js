8c7f5d8383ff7ebb989bf68241abe08a677ca6e7
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2020-12-16 23:48:57 -0400

    version 1

---
+++ assets/js/app.js
@@ -0,0 +1,116 @@
+/*========== SHOW MENU ==========*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    // Validate that variables exist
+    if(toggle && nav) {
+        // We add the show-menu class to the div tag with the nav__menu class
+        // Agregamos la clase show-menu a la etiqueta div con la clase nav__menu
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show-menu');
+        });
+    }
+};
+
+showMenu('nav-toggle', 'nav-menu');
+
+/*========== REMOVE MENU MOBILE ==========*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav_link, we remove the show-menu class
+    // Cuando hacemos clic en cada nav_link, eliminamos la clase show-menu
+    navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*========== SCROLL SECTIONS ACTIVE LINK ==========*/
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive); 
+
+/*==========    CHANGE BACKGROUND HEADER ==========*/
+function scrollHeader() {
+    const nav = document.getElementById('header');
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    // Cuando el desplazamiento tiene una altura superior a 200 viewport, agregue la clase de scroll-header a la etiqueta de encabezado
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header'); 
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+/*========== SHOW SCROLL TOP ==========*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    // Cuando el desplazamiento es superior a la  altura 560 de la ventana grafica, agregue la clase show-scroll a la etiqueta a con la clase scroll-top
+    if (this.scrollY >= 560) scrollTop.classList.add('scroll-top'); else scrollTop.classList.remove('scroll-top');
+}
+
+window.addEventListener('scroll', scrollTop);
+
+/*========== DARK LIGHT THEME ==========*/
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark':'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon':'bx-sun';
+
+// We validate if the user previously a topic
+if(selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activa
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+
+}
+// Activate / Desactivate  the theme manually 
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+/*========== SCROLL REVEAL ANIMATION ==========*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 1000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img, 
+            .about__data, .about__img, 
+            .services__content, .menu__content, 
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+});
